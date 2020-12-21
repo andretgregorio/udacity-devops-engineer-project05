483d2c990334ecb1568d0282defee82fca688d91
André T Gregório <andretgregorio@gmail.com>
2020-12-21 15:16:54 -0300

    Add docker commands to build and run container

--- run_docker.sh
+++ run_docker.sh
@@ -3,10 +3,10 @@
 ## Complete the following steps to get Docker running locally
 
 # Step 1:
-# Build image and add a descriptive tag
+docker build -t udacity-devops-project05 .
 
 # Step 2: 
-# List docker images
+docker image ls
 
 # Step 3: 
-# Run flask app
+docker run udacity-devops-project05
