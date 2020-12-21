38362dca6e6614e5a74ea3d3e0c25e42d80c24db
André T Gregório <andretgregorio@gmail.com>
2020-12-21 16:50:09 -0300

    [TASK04] Upload docker image to docker hub

--- upload_docker.sh
+++ upload_docker.sh
@@ -5,11 +5,15 @@
 
 # Step 1:
 # Create dockerpath
-# dockerpath=<your docker ID/path>
+dockerpath=542498/personal
+image=udacity-devops-project05
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+docker login --username 542498
+docker tag $image $dockerpath:1.0.0
 
 # Step 3:
 # Push image to a docker repository
+docker push $dockerpath
