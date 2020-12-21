ca1977c5ac1e6cabc8b8280a515ebcbfc818f890
André T Gregório <andretgregorio@gmail.com>
2020-12-21 17:15:07 -0300

    [TASK06] Deploy to Kubernets

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -4,15 +4,16 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath=542498/personal
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl create deployment udacity-devops --image=$dockerpath --port=80
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward deployment/udacity-devops 8000:80
